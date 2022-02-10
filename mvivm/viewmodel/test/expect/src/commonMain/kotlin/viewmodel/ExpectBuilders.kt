e7a26004da489fe77eae7c94c3bfdd6542aa9d99
andylamax <andylamax@programmer.net>
2022-02-10 04:19:31 +0300

    Fixed failing build

--- mvivm/viewmodel/test/expect/src/commonMain/kotlin/viewmodel/ExpectBuilders.kt
+++ mvivm/viewmodel/test/expect/src/commonMain/kotlin/viewmodel/ExpectBuilders.kt
@@ -5,14 +5,15 @@ package viewmodel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import live.WatchMode
 import kotlin.jvm.JvmName
 
 fun <I, S, V : ViewModel<I, S>> expect(viewModel: V) = ViewModelExpectation(viewModel)
 
 suspend fun <I, S> ViewModel<I, S>.expect(intent: I, with: CoroutineDispatcher = Dispatchers.Default): ViewModelStateExpectation<S> {
     val states = mutableListOf<S>()
-    val watcher = ui.watch { states.add(it) }
+    val watcher = ui.watch(WatchMode.CASUALLY) { states.add(it) }
     withContext(with) { start(intent) }
     watcher.stop()
-    return ViewModelStateExpectation(states.takeLast(states.size - 1))
+    return ViewModelStateExpectation(states)
 }
